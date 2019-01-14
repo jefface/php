26ccf30a4295b0addc6b8cb056300ae1fa525039
jefface <38710070+jefface@users.noreply.github.com>
2019-01-14 08:53:02 +0800

    大量更改

--- abc/shopp.php
+++ abc/shopp.php
@@ -1,9 +1,6 @@
 <?php
-$sql = "SELECT * FROM commodity ;
+$sql = "SELECT * FROM commodity" ;
 $record = mysql_query($sql);
 $row = mysql_fetch_assoc($record);
-while ($a <= $row) {
-    $$row[""]
-    $a++;
-}
->
+echo $row;
+?>
